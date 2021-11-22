cbad29e81d2eb344440329cd537f29e059b8f9e8
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-22 15:10:26 +0100

    add firstTitle

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <firstTitle />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firstTitle from './components/firstTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    firstTitle,
   }
 }
 </script>
